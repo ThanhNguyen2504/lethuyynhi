d7691699c86bbaabfe845e05fd3900f8289e0f87
lethuyynhi <nguyenthuanthanh2504@gmail.com>
2023-10-07 21:14:00 +0700

    profile

---
+++ js/typing-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+         strings: ["Nội thất.", "Ngoại thất.","Văn phòng.","Sân khấu."],
+    typeSpeed:40,
+        backSpeed:40,
+        loop:true,
+      });
